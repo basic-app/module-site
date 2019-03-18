bede5c4fa15976146b016d8023a1397f669d5406
basic-app <support@basic-app.com>
2019-03-19 00:43:07 +0300

    n/a

--- Helpers/block_helper.php
+++ Helpers/block_helper.php
@@ -4,8 +4,12 @@ use BasicApp\Site\Models\BlockModel;
 
 if (!function_exists('block'))
 {
-    function block(string $uid, string $default = '', $create = true)
+    function block(string $uid, string $default = '', array $params = [])
     {
-    	return BlockModel::content($uid, $create, ['block_content' => $default]);
+    	$return = BlockModel::content($uid, true, ['block_content' => $default]);
+
+        $return = strtr($return, $params);
+
+        return $return;
     }
 }
