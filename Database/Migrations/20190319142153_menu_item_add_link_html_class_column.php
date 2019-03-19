d52e7a054cc2fd588f7b9fb9fdd558f23ffa3fc2
basic-app <support@basic-app.com>
2019-03-19 21:27:59 +0300

    n/a

---
+++ Database/Migrations/20190319142153_menu_item_add_link_html_class_column.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace BasicApp\Site\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Migration_menu_item_add_link_html_class_column extends Migration
+{
+
+    public $tableName = 'menu_item';
+
+    public function up()
+    {
+        $this->forge->addColumn($this->tableName, [
+            'link_html_class' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255
+                'default' => null
+            ]
+        ]);
+    }
+
+    public function down()
+    {
+       $this->forge->dropColumn($this->tableName, 'link_html_class');
+    }
+
+}
