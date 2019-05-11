03c37a2cccc561d447b67e380703464d17d88009
basic-app <dev@basic-app.com>
2019-05-11 23:59:35 +0300

    n/a

--- Database/Migrations/20190323070931_menu_item_create_uid_unique_key.php
+++ Database/Migrations/20190323070931_menu_item_create_uid_unique_key.php
@@ -7,16 +7,16 @@ class Migration_menu_item_create_uid_unique_key extends \BasicApp\Core\Migration
 
     public $tableName = 'menu_item';
 
-    public $keyName = 'menu_item';
+    public $columns = ['item_uid', 'item_menu_id'];
 
     public function up()
     {
-        $this->tableAddKey($this->tableName, $this->keyName . '_item_uid_unique', ['item_uid', 'item_menu_id'], false, true);
+        $this->tableAddKey($this->tableName, $this->columns, false, true);
     }
 
     public function down()
     {
-        $this->tableDropKey($this->tableName, $this->keyName . '_item_uid_unique');
+        $this->tableDropKey($this->tableName, $this->keyName($this->tableName, $this->columns));
     }
 
 }
--- Database/Migrations/20190323114752_menu_item_create_item_enabled_index.php
+++ Database/Migrations/20190323114752_menu_item_create_item_enabled_index.php
@@ -11,12 +11,12 @@ class Migration_menu_item_create_item_enabled_index extends \BasicApp\Core\Migra
 
     public function up()
     {
-        $this->tableAddKey($this->tableName, $this->keyName . '_item_enabled_index', ['item_enabled'], false, true);
+        $this->tableAddKey($this->tableName, ['item_enabled'], false, true);
     }
 
     public function down()
     {
-        $this->tableDropKey($this->tableName, $this->keyName . '_item_enabled_index');
+        $this->tableDropKey($this->tableName, $this->keyName($this->tableName, ['item_enabled']));
     }
 
 }
