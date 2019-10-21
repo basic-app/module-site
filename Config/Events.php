5a4a3407f7b04c1fd69945c283fda3f95fbb12dc
basic-app <dev@basic-app.com>
2019-10-21 15:59:45 +0300

    n/a

--- Config/Events.php
+++ Config/Events.php
@@ -9,11 +9,11 @@ SystemEvents::onPreSystem(function()
     helper(['block', 'menu']);
 });
 
-AdminEvents::onMainMenu(function($menu)
+AdminEvents::onMainMenu(function($event)
 {
     if (BasicApp\Site\Controllers\Admin\Page::checkAccess())
     {
-        $menu->items['pages'] = [
+        $event->items['pages'] = [
             'url'   => Url::createUrl('admin/page'),
             'label' => t('admin.menu', 'Pages'),
             'icon'  => 'fa fa-file-text'
@@ -22,7 +22,7 @@ AdminEvents::onMainMenu(function($menu)
 
     if (BasicApp\Site\Controllers\Admin\Block::checkAccess())
     {
-        $menu->items['blocks'] = [
+        $event->items['blocks'] = [
             'url'   => Url::createUrl('admin/block'),
             'label' => t('admin.menu', 'Blocks'),
             'icon'  => 'fa fa-th'
@@ -31,7 +31,7 @@ AdminEvents::onMainMenu(function($menu)
 
     if (BasicApp\Site\Controllers\Admin\Menu::checkAccess())
     {
-        $menu->items['menu'] = [
+        $event->items['menu'] = [
             'url' => Url::createUrl('admin/menu'),
             'label' => t('admin.menu', 'Menu'),
             'icon' => 'fa fa-list-ul'
