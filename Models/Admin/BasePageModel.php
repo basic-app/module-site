5f57fa1b07ee5cac545ec8aa31bb25257ee3f00c
basic-app <dev@basic-app.com>
2019-11-11 22:57:31 +0300

    n/a

--- Models/Admin/BasePageModel.php
+++ Models/Admin/BasePageModel.php
@@ -20,7 +20,7 @@ abstract class BasePageModel extends \BasicApp\Site\Models\PageModel
 
 	protected $validationRules = [
 		'page_name' => 'trim|max_length[255]|required',
-		'page_url' => 'trim|max_length[255]|is_unique[pages.page_url,page_id,{page_id}]required',
+		'page_url' => 'trim|max_length[255]|is_unique[pages.page_url,page_id,{page_id}]',
 		'page_text' => 'trim|max_length[65535]',
 		'page_published' => 'in_list[0,1]'
 	];
