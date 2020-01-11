6057344c971e2dcc8217e723b2912b96489892ba
basic-app <dev@basic-app.com>
2020-01-11 18:46:32 +0300

    n/a

--- Views/Admin/Menu/_common.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$this->data['title'] = t('admin.menu', 'Menu');
-
-$this->data['mainMenu']['menu']['active'] = true;
-
-$this->data['breadcrumbs'][] = ['label' => $this->data['title'], 'url' => site_url('/admin/menu')];
